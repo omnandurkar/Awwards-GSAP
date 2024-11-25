42967aee3b24113ec56ce54cda43bec02605aa2a
omnandurkar <nandurkarom172@gmail.com>
2024-11-25 09:05:46 +0530

    ref: added basics

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Hero from './components/Hero'
+
+function App() {
+  
+
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+      <Hero />
+    </main>
+  )
+}
+
+export default App
