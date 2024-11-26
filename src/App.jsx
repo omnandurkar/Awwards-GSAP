65f5d51d34c79a323a0218550ccc0329679a4214
omnandurkar <nandurkarom172@gmail.com>
2024-11-26 07:12:46 +0530

    ref: added the features section with bento cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+
 import './App.css'
+import About from './components/About'
+import Features from './components/Features'
 import Hero from './components/Hero'
+import NavBar from './components/Navbar'
 
 function App() {
-  
+
 
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <NavBar />
       <Hero />
+      <About />
+      <Features />
     </main>
   )
 }
